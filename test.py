cf85cf672cc5b222081b75d69b0a1aa81f73e255
feiperam <feliperamirez5689@gmail.com>
2024-01-11 17:41:00 -0800

    vs code

---
+++ test.py
@@ -0,0 +1,7 @@
+if _name_ == '_main_':
+    x = "ECE_180_DA_DB"
+    if x == "EE_180_DA_DB":
+        PRINT("You are living in 2017")
+    else:
+        x = x + " - Best class ever"
+        print(x)
